890670bc84d042900bde99a57ab41f42be87fde6
ajaybajwa <ajaydeep1717@gmail.com>
2024-09-10 15:39:42 -0400

    Added SmartXPhones Logo at login screen

--- ecommerce-frontend/src/app/login/login.component.ts
+++ ecommerce-frontend/src/app/login/login.component.ts
@@ -11,7 +11,7 @@ export class LoginComponent implements OnInit {
   oktaSignin: any;
   constructor(private oktaAuthService:OktaAuthService) {
     this.oktaSignin = new OktaSignIn({
-      logo: '',
+      logo: 'https://res.cloudinary.com/ajaybajwa/image/upload/v1724723419/Ecommerce/wpx8efjh1q1ysoohw5ex.png',
       baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
       clientId: myAppConfig.oidc.clientId,
       redirectUri: myAppConfig.oidc.redirectUri,
